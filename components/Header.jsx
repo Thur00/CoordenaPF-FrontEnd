b4a109dd176c8c2dfd4d721d684b60083c3aac4a
sofiacaunfm <sofiacaumfm@gmail.com>
2024-09-23 10:19:29 -0300

    Componente Header

---
+++ components/Header.jsx
@@ -0,0 +1,27 @@
+"use client"
+import Link from "next/link"
+function Header(props) {
+    return (
+        <main>
+            <div className="Header">
+
+                <Link className="icon" href="https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons">
+                    <i class="fa-solid fa-house-chimney"></i>
+                </Link>
+
+                <h1 className="tit">Coordena SESI</h1>
+                <div>
+                    <Link className="icon2" href="https://quizizz.com/">
+                        <i class="fa-regular fa-bell"></i>
+                    </Link>
+
+                    <Link className="icon2" href="https://www.youtube.com/?app=desktop&hl=pt">
+                        <i class="fa-solid fa-user"></i>
+                    </Link>
+                </div>
+            </div>
+
+        </main>
+    )
+}
+export default Header
