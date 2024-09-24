72bd779c605c4bcf5d317c286b956e10d40bd3ac
gabyycocielli <129217256+gabyycocielli@users.noreply.github.com>
2024-09-24 14:48:27 -0300

    .

--- app/Paginas/EditarDados/page.jsx
+++ app/Paginas/EditarDados/page.jsx
@@ -2,33 +2,51 @@
 
 import React from "react";
 import Link from "next/link";
+import Footer from "@/Components/Footer";
+import Header from "@/Components/Header";
+import styles from "@/Components/EditarDados.module.css"
 
 
-const EditarDados = () => {
+function EditarDados() {
     return (
         <div>
-        <button>
+            <Header></Header>
+            <br></br>
+
+            <h1 className={styles.title}>Editar Dados</h1>
+
+            <div className={styles.divbutton}>
+        <button className={styles.button}>
         <Link href="/editaraluno">Editar Aluno</Link>
         </button>
 
-        <button>
+        <button className={styles.button}>
         <Link href="/editaraspecto">Editar Aspecto</Link>
         </button>
 
-        <button>
+        <button className={styles.button}>
         <Link href="/editartema">Editar Tema</Link>
         </button>
 
-        <button>
+        <button className={styles.button}>
         <Link href="/editarstatus">Editar Status</Link>
         </button>
 
-        <button>
+        <button className={styles.button}>
         <Link href="/editarurgencia">Editar Urgência</Link>
         </button>
 
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <Footer></Footer>
 
         </div>
+
+
 
     )
 }
