7946d90dac1e04480862867c9b82a53c3ac531d0
Pyh Quinquio <pyetraquinquio2007@gmail.com>
2024-09-24 16:31:09 -0300

    footer

--- app/Paginas/usuarios/page.jsx
+++ app/Paginas/usuarios/page.jsx
@@ -13,8 +13,7 @@ export default function Home() {
 <br></br>
  <div className={styles.bt}>
  <h1 className={styles.h1}>Usuário</h1>   
-<button className={styles.b1} >Adicionar usuário </button>
-{/* <i class="material-icons">add_circle_outline</i> */}
+<button className={styles.b1} >Adicionar usuário</button>
 </div>
 
 <div className={styles.caixas}>
