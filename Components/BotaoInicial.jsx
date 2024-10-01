c0ebda9d1a7ca77f39370726568846f14c6bdd46
Maria Eduarda <eduarda.maria140806@gmail.com>
2024-10-01 07:57:42 -0300

    ...

--- Components/BotaoInicial.jsx
+++ Components/BotaoInicial.jsx
@@ -10,8 +10,8 @@ function BotaoInicial() {
  return (
         <>
            
-    <Link href="https://quizizz.com/" ><button  className={styles.b1}><CgAdd className={styles.icon}></CgAdd>Criar ocorrência </button></Link>
-    <Link href="https://quizizz.com/" ><button  className={styles.b1}><CgFileDocument className={styles.icon}></CgFileDocument>Ocorrência </button></Link>
+    <Link href="/Paginas/CriarOcorrencia" ><button  className={styles.b1}><CgAdd className={styles.icon}></CgAdd>Criar ocorrência </button></Link>
+    <Link href="/Paginas/TodasOcorrencias" ><button  className={styles.b1}><CgFileDocument className={styles.icon}></CgFileDocument>Ocorrência </button></Link>
     <Link href="https://quizizz.com/" ><button  className={styles.b1}><CgAdd className={styles.icon}></CgAdd>Editar Dados </button></Link>
          
           
