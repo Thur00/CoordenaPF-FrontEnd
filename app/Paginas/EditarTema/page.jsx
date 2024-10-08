7da80fc995ef1cd073decb9885f0e38f0fdd2abb
Thur00 <arthursvinhedo@gmail.com>
2024-10-08 15:43:47 -0300

    modelo exmplo das telas crud

--- app/Paginas/EditarTema/page.jsx
+++ app/Paginas/EditarTema/page.jsx
@@ -1,94 +1,132 @@
-
-
 "use client";
 
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "@/Components/Adicionar.module.css"; // Importando o CSS
-import Header from "@/Components/Header";
-import Link from "next/link"
-import Footer from "@/Components/Footer";
+import Link from "next/link";
 
+const API_URL = "http://localhost:3001"; // Adicione a URL da API
 
 const Tabela = () => {
-    const [data, setData] = useState([]);
-    const [formData, setFormData] = useState({ id: "", tema: "" });
-    const [isEditing, setIsEditing] = useState(false);
-    const [showForm, setShowForm] = useState(false);
-    const [editingItem, setEditingItem] = useState(null);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [formData, setFormData] = useState({ id: "", tema: "" });
+  const [isEditing, setIsEditing] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [editingItem, setEditingItem] = useState(null);
 
-    useEffect(() => {
-        const getTema = async () => {
-          try {
-            const resposta = await fetch(`http://localhost:3001/temas`);
-            const data1 = await resposta.json();
-            console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
-            setData(data1);
-            setError(null);
-          } catch (error) {
-            console.error("Erro na busca alunos", error);
-            setError("Falha na busca alunos. Tente novamente.");
-          }
-        };
+  const getTema = async () => {
+    try {
+      const resposta = await fetch(`${API_URL}/temas`);
+      const data1 = await resposta.json();
+      console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
+      setData(data1);
+      setError(null);
+    } catch (error) {
+      console.error("Erro na busca alunos", error);
+    }
+  };
+
+  useEffect(() => {
+    getTema();
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleAdd = () => {
+    setShowForm(true);
+    setIsEditing(false);
+    setFormData({ id: "", tema: "" });
+  };
+
+  const handleEdit = (item) => {
+    setShowForm(true);
+    setIsEditing(true);
+    setFormData({ tema: item.Nome_tema });  
+    setEditingItem(item);
+  };
+
+  const handleSave = async () => {
+    if (isEditing) {
+      try {
+        // Faz uma requisição PUT para a API de temas para atualizar o item
+        await fetch(`${API_URL}/temas/${editingItem.Tema_id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ Nome_tema: formData.tema }), // Ajuste aqui o objeto para corresponder ao que a API espera
+        });
+
+        // Atualiza a lista de temas após a edição
         getTema();
-      }, []);
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
-
-    const handleAdd = () => {
-        setShowForm(true);
+        // Limpa a seleção e o formulário
+        setEditingItem(null);
         setIsEditing(false);
+      } catch (error) {
+        console.error("Erro ao atualizar o tema:", error);
+      }
+    } else {
+      try {
+        // Faz uma requisição POST para a API de temas
+        const response = await fetch(`${API_URL}/temas`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // Envia o corpo da requisição em formato JSON
+          body: JSON.stringify({ Nome_tema: formData.tema }),
+        });
+
+        // Atualiza a lista de temas após a edição
+        getTema();
+
+        // Converte a resposta para JSON
+        const data = await response.json();
+
+        // Atualiza o estado de 'data' com o novo tema adicionado
+        setData((prevData) => [...prevData, data]);
+
+        // Limpa os campos de entrada
         setFormData({ id: "", tema: "" });
-    };
-
-    const handleEdit = (item) => {
-        setShowForm(true);
-        setIsEditing(true);
-        setFormData(item);
-        setEditingItem(item);
-    };
-
-    const handleSave = () => {
-        if (isEditing) {
-            setData(
-                data.map((item) => (item.id === editingItem.id ? formData : item))
-            );
-        } else {
-            setData([...data, { ...formData, rm: Number(formData.rm) }]);
-        }
         setShowForm(false);
-        setFormData({ id: "", tema: "" });
-    };
+      } catch (error) {
+        // Loga erros no console
+        console.error("Erro ao adicionar tema:", error);
+      }
+    }
+    setShowForm(false);
+    setFormData({ id: "", tema: "" });
+  };
 
-    const handleCancel = () => {
-        setShowForm(false);
-        setFormData({ id: "", tema: "" });
-    };
+  const handleCancel = () => {
+    setShowForm(false);
+    setFormData({ id: "", tema: "" });
+  };
 
-    return (
-        <div>
-            <Header></Header>
-            <br></br>
-            <div>
-                <div className={styles.div1}>
-                    <h1 className={styles.h1}>Editar Tema</h1>
-                    <button className={styles.voltar}>
-                        <Link href="/Paginas/EditarDados">Voltar</Link>
-                    </button>         </div>
-                <br></br>
-                <div className={styles.div2}>
-                    <table border="1" className={styles.table}>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>TEMA</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {data.length > 0 ? (
+  return (
+    <div>
+      <br></br>
+      <div>
+        <div className={styles.div1}>
+          <h1 className={styles.h1}>Editar Aspecto</h1>
+          <button className={styles.voltar}>
+            <Link href="/Paginas/EditarDados">Voltar</Link>
+          </button>
+        </div>
+        <br></br>
+        <div className={styles.div2}>
+          <table border="1" className={styles.table}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Tema</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length > 0 ? (
                 data.map((item) => (
                   <tr key={item.Tema_id}>
                     <td>{item.Tema_id}</td>
@@ -97,49 +135,57 @@ const Tabela = () => {
                 ))
               ) : (
                 <tr>
-                  <td colSpan="5">Nenhum tema encontrado.</td>
+                  <td colSpan="2">Nenhum tema encontrado.</td>
                 </tr>
               )}
-                        </tbody>
-                    </table>
+            </tbody>
+          </table>
 
-                    <div className={styles.buttonContainer}>
-                        {data.map((item) => (
-                            <button className={styles.editarbutton} key={item.rm} onClick={() => handleEdit(item)}>
-                                Editar
-                            </button>
-                        ))}
-                    </div>
-                </div>
-            </div>
-
-            <button className={styles.addbutton} onClick={handleAdd}>Adicionar</button>
-
-            {showForm && (
-                <div>
-                    <h3>{isEditing ? "" : ""}</h3>
-                    <br></br>
-                    <div className={styles.divinput}>
-
-                        <input
-                            type="text"
-                            name="tema"
-                            value={formData.tema}
-                            onChange={handleInputChange}
-                            placeholder="Tema"
-                        />
-
-                    </div>
-
-                    <div className={styles.salecanbutton}>
-                        <button className={styles.salvarbutton} onClick={handleSave}>Salvar</button>
-                        <button className={styles.cancelarbutton} onClick={handleCancel}>Cancelar</button>
-                    </div>
-                </div>
-            )}
-            <Footer></Footer>
+          <div className={styles.buttonContainer}>
+            {data.map((item) => (
+              <button
+                className={styles.editarbutton}
+                key={item.id}
+                onClick={() => handleEdit(item)}
+              >
+                Editar
+              </button>
+            ))}
+          </div>
         </div>
-    );
+      </div>
+
+      <button className={styles.addbutton} onClick={handleAdd}>
+        Adicionar
+      </button>
+
+      {showForm && (
+        <div>
+          <h3 className={styles.titleinput}>
+            {isEditing ? "Editar" : "Adicionar"}
+          </h3>
+          <div className={styles.divinput}>
+            <input
+              type="text"
+              name="tema"
+              value={formData.tema}
+              onChange={handleInputChange}
+              placeholder="tema"
+            />
+          </div>
+
+          <div className={styles.salecanbutton}>
+            <button className={styles.salvarbutton} onClick={handleSave}>
+              Salvar
+            </button>
+            <button className={styles.cancelarbutton} onClick={handleCancel}>
+              Cancelar
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Tabela;
