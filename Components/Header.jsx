23dbfcee0042e0854a88cf8381cc3ee508e0616f
AnaBevi <bevilaquaanab@gmail.com>
2024-10-08 10:51:22 -0300

    Alterações ana

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,6 +1,8 @@
 "use client"
 import Link from "next/link"
-
+import { FaHouseChimney } from "react-icons/fa6";
+import { FaBell } from "react-icons/fa";
+import { IoPersonSharp } from "react-icons/io5";
 
 function Header(props) {
     return (
@@ -8,17 +10,17 @@ function Header(props) {
             <div className="Header">
 
                 <Link className="icon" href="https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons">
-                    <i class="fa-solid fa-house-chimney"></i>
+                <FaHouseChimney />
                 </Link>
 
                 <h1 className="tit">Coordena SESI</h1>
                 <div>
                     <Link className="icon2" href="https://quizizz.com/">
-                        <i class="fa-solid fa-bell"></i>
+                    <FaBell />
                     </Link>
 
                     <Link className="icon2" href="https://www.youtube.com/?app=desktop&hl=pt">
-                        <i class="fa-solid fa-user"></i>
+                    <IoPersonSharp />
                     </Link>
                 </div>
             </div>
