45db894b299a51075b1bcd685f420444747f64d3
AnaBevi <bevilaquaanab@gmail.com>
2024-10-08 16:26:00 -0300

    status

--- app/Paginas/EditarStatus/page.jsx
+++ app/Paginas/EditarStatus/page.jsx
@@ -3,13 +3,10 @@
 import { useState } from "react";
 import styles from "@/Components/Adicionar.module.css"; // Importando o CSS
 import Header from "@/Components/Header";
-import Link from "next/link"
+import Link from "next/link";
 import Footer from "@/Components/Footer";
 
-const initialData = [
-    { id: 1, categoria: "concluido", icone: "sla" },
-    { id: 2, categoria: "em andamento", icone: "naosei" },
-];
+const API_URL = "http://localhost:3001"; // Adicione a URL da API
 
 const Tabela = () => {
     const [data, setData] = useState(initialData);
@@ -18,119 +15,193 @@ const Tabela = () => {
     const [showForm, setShowForm] = useState(false);
     const [editingItem, setEditingItem] = useState(null);
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const getStatus = async () => {
+    try {
+      const resposta = await fetch(`${API_URL}/status`);
+      const data1 = await resposta.json();
+      console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
+      setData(data1);
+      setError(null);
+    } catch (error) {
+      console.error("Erro na busca", error);
+    }
+  };
 
-    const handleAdd = () => {
-        setShowForm(true);
+  useEffect(() => {
+    getStatus();
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleAdd = () => {
+    setShowForm(true);
+    setIsEditing(false);
+    setFormData({ id: "", categoria: "", icone: "" });
+  };
+
+  const handleEdit = (item) => {
+    setShowForm(true);
+    setIsEditing(true);
+    setFormData(item);
+    setEditingItem(item);
+};
+
+  const handleSave = async () => {
+    if (isEditing) {
+      try {
+        // Faz uma requisição PUT para a API de temas para atualizar o item
+        await fetch(`${API_URL}/status/${editingItem.Status_id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ Categoria: formData.categoria, Icone: formData.icone }), // Ajuste aqui o objeto para corresponder ao que a API espera
+        });
+
+        // Atualiza a lista de temas após a edição
+        getStatus();
+
+        // Limpa a seleção e o formulário
+        setEditingItem(null);
         setIsEditing(false);
+      } catch (error) {
+        console.error("Erro ao atualizar o status:", error);
+      }
+    } else {
+      try {
+        // Faz uma requisição POST para a API de temas
+        const response = await fetch(`${API_URL}/status`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // Envia o corpo da requisição em formato JSON
+          body: JSON.stringify({ Categoria: formData.categoria }),
+        });
+
+        // Atualiza a lista de temas após a edição
+        getStatus();
+
+        // Converte a resposta para JSON
+        const data = await response.json();
+
+        // Atualiza o estado de 'data' com o novo tema adicionado
+        setData((prevData) => [...prevData, data]);
+
+        // Limpa os campos de entrada
         setFormData({ id: "", categoria: "", icone: "" });
-    };
-
-    const handleEdit = (item) => {
-        setShowForm(true);
-        setIsEditing(true);
-        setFormData(item);
-        setEditingItem(item);
-    };
-
-    const handleSave = () => {
-        if (isEditing) {
-            setData(
-                data.map((item) => (item.id === editingItem.id ? formData : item))
-            );
-        } else {
-            setData([...data, { ...formData, id: Number(formData.id) }]);
-        }
         setShowForm(false);
-        setFormData({ id: "", categoria: "", icone: "" });
-    };
+      } catch (error) {
+        // Loga erros no console
+        console.error("Erro ao adicionar status:", error);
+      }
+    }
+    setShowForm(false);
+    setFormData({ id: "", categoria: "", icone: "" });
+  };
 
-    const handleCancel = () => {
-        setShowForm(false);
-        setFormData({ id: "", categoria: "", icone: "" });
-    };
+  const handleCancel = () => {
+    setShowForm(false);
+    setFormData({ id: "", categoria: "", icone: "" });
+  };
 
-    return (
-        <div>
-            <Header></Header>
-            <br></br>
-            <div>
-                <div className={styles.div1}>
-                    <h1 className={styles.h1}>Editar Status</h1>
-                    <button className={styles.voltar}>
-                        <Link href="/Paginas/EditarDados">Voltar</Link>
-                    </button>         </div>
-                <br></br>
-                <div className={styles.div2}>
-                    <table border="1" className={styles.table}>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>CATEGORIA</th>
-                                <th>ICONE</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((item) => (
-                                <tr key={item.id}>
-                                    <td>{item.id}</td>
-                                    <td>{item.categoria}</td>
-                                    <td>{item.icone}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-
-                    <div className={styles.buttonContainer}>
-                        {data.map((item) => (
-                            <button className={styles.editarbutton} key={item.id} onClick={() => handleEdit(item)}>
-                                Editar
-                            </button>
-                        ))}
-                    </div>
-                </div>
-            </div>
-
-            <button className={styles.addbutton} onClick={handleAdd}>Adicionar</button>
-
-            {showForm && (
-                <div>
-                    <h3>{isEditing ? "" : ""}</h3>
-                    <br></br>
-                    <div className={styles.divinput}>
-                        <br></br>
-                        <input
-                            type="text"
-                            name="categoria"
-                            value={formData.categoria}
-                            onChange={handleInputChange}
-                            placeholder="Categoria"
-                        />
-                        <br></br>
-
-                        <input
-                            type="text"
-                            name="icone"
-                            value={formData.icone}
-                            onChange={handleInputChange}
-                            placeholder="Icone"
-                        />
-                        <br></br>
-
-                    </div>
-
-                    <div className={styles.salecanbutton}>
-                        <button className={styles.salvarbutton} onClick={handleSave}>Salvar</button>
-                        <button className={styles.cancelarbutton} onClick={handleCancel}>Cancelar</button>
-                    </div>
-                </div>
-            )}
-            <Footer></Footer>
+  return (
+    <div>
+        <Header></Header>
+      <br></br>
+      <div>
+        <div className={styles.div1}>
+          <h1 className={styles.h1}>Editar Status</h1>
+          <button className={styles.voltar}>
+            <Link href="/Paginas/EditarDados">Voltar</Link>
+          </button>
         </div>
-    );
+        <br></br>
+        <div className={styles.div2}>
+          <table border="1" className={styles.table}>
+            <thead>
+              <tr>
+              <th>ID</th>
+              <th>CATEGORIA</th>
+              <th>ICONE</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length > 0 ? (
+                data.map((item) => (
+                  <tr key={item.Status_id}>
+                    <td>{item.Status_id}</td>
+                    <td>{item.Categoria}</td>
+                    <td>{item.icone}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="2">Nenhum Status encontrado.</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+
+          <div className={styles.buttonContainer}>
+            {data.map((item) => (
+              <button
+                className={styles.editarbutton}
+                key={item.id}
+                onClick={() => handleEdit(item)}
+              >
+                Editar
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <button className={styles.addbutton} onClick={handleAdd}>
+        Adicionar
+      </button>
+
+      {showForm && (
+        <div>
+          <h3 className={styles.titleinput}>
+            {isEditing ? "Editar" : "Adicionar"}
+          </h3>
+          <div className={styles.divinput}>
+            <input
+              type="text"
+              name="categoria"
+              value={formData.categoria}
+              onChange={handleInputChange}
+              placeholder="Categoria"
+            />
+            <br></br>
+
+<input
+    type="text"
+    name="icone"
+    value={formData.icone}
+    onChange={handleInputChange}
+    placeholder="Icone"
+/>
+<br></br>
+          </div>
+
+          <div className={styles.salecanbutton}>
+            <button className={styles.salvarbutton} onClick={handleSave}>
+              Salvar
+            </button>
+            <button className={styles.cancelarbutton} onClick={handleCancel}>
+              Cancelar
+            </button>
+          </div>
+        </div>
+      )}
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Tabela;
