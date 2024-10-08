fd87d9b9b4a6873d9dea7e0dc7635e38a1c27fbc
Emily <emily.rsilva@hotmail.com>
2024-10-08 16:26:45 -0300

    apiAluno com bd

--- app/Paginas/EditarAluno/page.jsx
+++ app/Paginas/EditarAluno/page.jsx
@@ -2,34 +2,34 @@
 
 import { useEffect, useState } from "react";
 import styles from "@/Components/Adicionar.module.css"; // Importando o CSS
-import Header from "@/Components/Header";
 import Link from "next/link";
-import Footer from "@/Components/Footer";
+
+const API_URL = "http://localhost:3001"; // Adicione a URL da API
 
 const Tabela = () => {
   const [data, setData] = useState([]);
   const [formData, setFormData] = useState({
-    rm: "",
+     rm: "",
     nome: "",
     turma: "",
-    ano: "",
-  });
+    ano: "", });
   const [isEditing, setIsEditing] = useState(false);
   const [showForm, setShowForm] = useState(false);
   const [editingItem, setEditingItem] = useState(null);
 
+  const getAlunos = async () => {
+    try {
+      const resposta = await fetch(`${API_URL}/alunos`);
+      const data1 = await resposta.json();
+      console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
+      setData(data1);
+      setError(null);
+    } catch (error) {
+      console.error("Erro na busca alunos", error);
+    }
+  };
+
   useEffect(() => {
-    const getAlunos = async () => {
-      try {
-        const resposta = await fetch(`http://localhost:3001/alunos`);
-        const data1 = await resposta.json();
-        console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
-        setData(data1);
-        setError(null);
-      } catch (error) {
-        console.error("Erro na busca alunos", error);
-      }
-    };
     getAlunos();
   }, []);
 
@@ -47,17 +47,59 @@ const Tabela = () => {
   const handleEdit = (item) => {
     setShowForm(true);
     setIsEditing(true);
-    setFormData(item);
+    setFormData({ Nome: item.nome, Turma: item.turma, Ano: item.ano });  
     setEditingItem(item);
   };
 
-  const handleSave = () => {
+  const handleSave = async () => {
     if (isEditing) {
-      setData(
-        data.map((item) => (item.rm === editingItem.rm ? formData : item))
-      );
+      try {
+        // Faz uma requisição PUT para a API de temas para atualizar o item
+        await fetch(`${API_URL}/alunos/${editingItem.RM}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ Nome: formData.nome, Turma: formData.turma, Ano: formData.ano, }), // Ajuste aqui o objeto para corresponder ao que a API espera
+        });
+
+        // Atualiza a lista de temas após a edição
+        getAlunos();
+
+        // Limpa a seleção e o formulário
+        setEditingItem(null);
+        setIsEditing(false);
+      } catch (error) {
+        console.error("Erro ao atualizar o aluno:", error);
+      }
     } else {
-      setData([...data, { ...formData, rm: Number(formData.rm) }]);
+      try {
+        // Faz uma requisição POST para a API de temas
+        const response = await fetch(`${API_URL}/alunos`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // Envia o corpo da requisição em formato JSON
+          body: JSON.stringify({ RM: formData.rm }),
+        });
+
+        // Atualiza a lista de temas após a edição
+        getTema();
+
+        // Converte a resposta para JSON
+        const data = await response.json();
+
+        // Atualiza o estado de 'data' com o novo tema adicionado
+        setData((prevData) => [...prevData, data]);
+
+        // Limpa os campos de entrada
+        setFormData({ rm: "", nome: "", turma: "", ano: "" });
+        setShowForm(false);
+      } catch (error) {
+        // Loga erros no console
+        console.error("Erro ao adicionar aluno:", error);
+      }
     }
     setShowForm(false);
     setFormData({ rm: "", nome: "", turma: "", ano: "" });
@@ -83,7 +125,7 @@ const Tabela = () => {
           <table border="1" className={styles.table}>
             <thead>
               <tr>
-                <th>RM</th>
+              <th>RM</th>
                 <th>NOME</th>
                 <th>TURMA</th>
                 <th>ANO</th>
@@ -93,15 +135,15 @@ const Tabela = () => {
               {data.length > 0 ? (
                 data.map((item) => (
                   <tr key={item.RM}>
-                    <td>{item.RM}</td>
-                    <td>{item.Nome}</td>
-                    <td>{item.Turma}</td>
-                    <td>{item.Ano}</td>
+                  <td>{item.RM}</td>
+                  <td>{item.Nome}</td>
+                  <td>{item.Turma}</td>
+                  <td>{item.Ano}</td>
                   </tr>
                 ))
               ) : (
                 <tr>
-                  <td colSpan="5">Nenhum aluno encontrado.</td>
+                  <td colSpan="2">Nenhum aluno encontrado.</td>
                 </tr>
               )}
             </tbody>
@@ -127,41 +169,16 @@ const Tabela = () => {
 
       {showForm && (
         <div>
-          <h3>{isEditing ? "" : ""}</h3>
-          <br></br>
+          <h3 className={styles.titleinput}>
+            {isEditing ? "Editar" : "Adicionar"}
+          </h3>
           <div className={styles.divinput}>
-            <input
-              type="number"
-              name="rm"
-              value={formData.rm}
-              onChange={handleInputChange}
-              placeholder="RM"
-            />
-            <br></br>
             <input
               type="text"
               name="nome"
               value={formData.nome}
               onChange={handleInputChange}
               placeholder="Nome"
-            />
-            <br></br>
-
-            <input
-              type="text"
-              name="turma"
-              value={formData.turma}
-              onChange={handleInputChange}
-              placeholder="Turma"
-            />
-            <br></br>
-
-            <input
-              type="number"
-              name="ano"
-              value={formData.ano}
-              onChange={handleInputChange}
-              placeholder="Ano"
             />
           </div>
 
