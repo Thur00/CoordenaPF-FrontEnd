85837a3233a1207336b7d9d634a3e82c6a307e3e
Thur00 <arthursvinhedo@gmail.com>
2024-10-29 14:48:29 -0300

    commit pre branch

--- app/Paginas/AdicionarUsuario/page.jsx
+++ app/Paginas/AdicionarUsuario/page.jsx
@@ -4,124 +4,135 @@ import { useState } from "react";
 import styles from "@/Components/AddUsuario.module.css";
 import Link from "next/link";
 
+const Usuarios1 = [{ cargo: "", nome: "", email: "", senha: "", cpf: "" }];
 
-const Usuarios1 = [
-    { cargo: "", nome: "", email: "", senha: "", cpf: ""}
-  ];
-  
-  const Input1 = () => {
-    const [data, setData] = useState(Usuarios1);
-    const [focargoData, setFocargoData] = useState({ cargo: "", nome: "", email: "", ano: "", senha: "", cpf: "" });
-    const [isEditing, setIsEditing] = useState(false);
-    const [showFocargo, setShowFocargo] = useState(false);
-  
-    const handleInputChange = (e) => {
-      const { name, value } = e.target;
-      setFocargoData({ ...focargoData, [name]: value });
-    };
-  
-    const handleAdd = () => {
-      setShowFocargo(true);
-      setIsEditing(false);
-      setFocargoData({ cargo: "", nome: "", email: "", ano: "", senha: "", cpf: ""});
-    };
-  
-    const handleEdit = (item) => {
-      setShowFocargo(true);
-      setIsEditing(true);
-      setFocargoData(item);
-    };
-  
-    const handleSave = () => {
-      if (isEditing) {
-        setData(data.map((item) => (item.cargo === focargoData.cargo ? focargoData : item)));
-      } else {
-        setData([...data, { ...focargoData, cargo: data.length + 1 }]);
-      }
-      setShowFocargo(false);
-    };
-  
-    const handleCancel = () => {
-      setShowFocargo(false);
-      setFocargoData({ cargo: "", nome: "", email: "", senha: "", cpf: ""});
-    };
-  
-    return (
-      <div>
-          
-          <br></br>
-          <div className={styles.div1}>
-          <h1 className={styles.h1}>Adicionar Usuário</h1>
-          <button className={styles.voltar}>
+const Input1 = () => {
+  const [data, setData] = useState(Usuarios1);
+  const [focargoData, setFocargoData] = useState({
+    cargo: "",
+    nome: "",
+    email: "",
+    ano: "",
+    senha: "",
+    cpf: "",
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const [showFocargo, setShowFocargo] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFocargoData({ ...focargoData, [name]: value });
+  };
+
+  const handleAdd = () => {
+    setShowFocargo(true);
+    setIsEditing(false);
+    setFocargoData({
+      cargo: "",
+      nome: "",
+      email: "",
+      ano: "",
+      senha: "",
+      cpf: "",
+    });
+  };
+
+  const handleEdit = (item) => {
+    setShowFocargo(true);
+    setIsEditing(true);
+    setFocargoData(item);
+  };
+
+  const handleSave = () => {
+    if (isEditing) {
+      setData(
+        data.map((item) =>
+          item.cargo === focargoData.cargo ? focargoData : item
+        )
+      );
+    } else {
+      setData([...data, { ...focargoData, cargo: data.length + 1 }]);
+    }
+    setShowFocargo(false);
+  };
+
+  const handleCancel = () => {
+    setShowFocargo(false);
+    setFocargoData({ cargo: "", nome: "", email: "", senha: "", cpf: "" });
+  };
+
+  return (
+    <div>
+      <br></br>
+      <div className={styles.div1}>
+        <h1 className={styles.h1}>Adicionar Usuário</h1>
+        <button className={styles.voltar}>
           <Link href="/Paginas/Usuarios">Voltar</Link>
-          </button>
-          </div>
-          <br></br>
-           
-        
-          <div className={styles.divinput}>
-            <h3>{isEditing ? "" : ""}</h3>
-            <br></br>
-  
-            <input
-              type="text"
-              name="cargo"
-              value={focargoData.cargo}
-              onChange={handleInputChange}
-              placeholder="Cargo:"
-            />
-            <br></br>
-  
-  <input
-              type="text"
-              name="nome"
-              value={focargoData.nome}
-              onChange={handleInputChange}
-              placeholder="Nome:"
-            />
-            <br></br>
-  
-  <input
-              type="text"
-              name="email"
-              value={focargoData.email}
-              onChange={handleInputChange}
-              placeholder="E-mail:"
-            />
-            <br></br>
-  
-            <input
-              type="text"
-              name="senha"
-              value={focargoData.senha}
-              onChange={handleInputChange}
-              placeholder="Senha:"
-            />
-            <br></br>
-  <input
-              type="number"
-              name="cpf"
-              value={focargoData.cpf}
-              onChange={handleInputChange}
-              placeholder="CPF:"
-            />
-            <div className={styles.salecanbutton}>
-            <button className={styles.cancelarbutton} onClick={handleAdd}>Salvar</button>
-            <button className={styles.cancelarbutton} onClick={handleCancel}>Cancelar</button>
-          
-            </div>
-  
-  
-          </div>
-        
-        <br></br>
-        <br></br>
-       
-       
+        </button>
       </div>
-      
-  
-    );}
+      <br></br>
+
+      <div className={styles.divinput}>
+        <h3>{isEditing ? "" : ""}</h3>
+        <br></br>
+
+        <input
+          type="text"
+          name="cargo"
+          value={focargoData.cargo}
+          onChange={handleInputChange}
+          placeholder="Cargo:"
+        />
+        <br></br>
+
+        <input
+          type="text"
+          name="nome"
+          value={focargoData.nome}
+          onChange={handleInputChange}
+          placeholder="Nome:"
+        />
+        <br></br>
+
+        <input
+          type="text"
+          name="email"
+          value={focargoData.email}
+          onChange={handleInputChange}
+          placeholder="E-mail:"
+        />
+        <br></br>
+
+        <input
+          type="text"
+          name="senha"
+          value={focargoData.senha}
+          onChange={handleInputChange}
+          placeholder="Senha:"
+        />
+        <br></br>
+        <input
+          type="number"
+          name="cpf"
+          value={focargoData.cpf}
+          onChange={handleInputChange}
+          placeholder="CPF:"
+        />
+        <div className={styles.salecanbutton}>
+          <button className={styles.cancelarbutton} onClick={handleAdd}>
+            Salvar
+          </button>
+          <button className={styles.cancelarbutton} onClick={handleCancel}>
+            Cancelar
+          </button>
+        </div>
+      </div>
+
+      <br></br>
+      <br></br>
+    </div>
+  );
+};
 
 const Usuarios = [{ cargo: "", nome: "", email: "", senha: "", cpf: "" }];
 
@@ -141,7 +152,6 @@ const Input = () => {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFocargoData({ ...focargoData, [name]: value });
-
   };
 
   const handleAdd = () => {
--- app/Paginas/PaginaInicial/page.jsx
+++ app/Paginas/PaginaInicial/page.jsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import styles from "@/Components/InicialComponentes.module.css"
+import styles from "@/Components/InicialComponentes.module.css";
 import React from "react";
 import InicialOcorrencia from "@/Components/InicialOcorrencias";
 import AndamentoOcorrencias from "@/Components/AndamentoOcorrencias";
@@ -9,68 +9,48 @@ import { useEffect, useState } from "react";
 
 const API_URL = "http://localhost:3001";
 
-
-
 const inicial = () => {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
-  
-  
-    const getInicial = async () => {
-      try {
-        const resposta = await fetch(`${API_URL}/alunos`);
-        const data1 = await resposta.json();
-        console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
-        setData(data1);
-        setError(null);
-      } catch (error) {
-        console.error("Erro na busca alunos", error);
-      }
-    };
-  
-    useEffect(() => {
-      getInicial();
-    }, []);
-  
+  const getInicial = async () => {
+    try {
+      const resposta = await fetch(`${API_URL}/ocorrencias`);
+      const data1 = await resposta.json();
+      console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
+      setData(data1);
+      setError(null);
+    } catch (error) {
+      console.error("Erro na busca alunos", error);
+    }
+  };
 
-  
-    return (
-        <div>
-           
+  useEffect(() => {
+    getInicial();
+  }, []);
 
-            <br></br>
+  return (
+    <div>
+      <br></br>
 
-            <div className={styles.bt}>
+      <div className={styles.bt}>
+        <BotaoInicial></BotaoInicial>
+      </div>
 
-   <BotaoInicial></BotaoInicial>
-          
-</div>
+      <br></br>
+      <br></br>
 
-<br></br>
-<br></br>
+      <div className={styles.tudo}>
+        <h2 className={styles.h4}>Notificações</h2>
+        <h1 className={styles.titulo}>Você foi mencionado recentemente:</h1>
 
-<div className={styles.tudo}>
-<h2 className={styles.h4}>Notificações</h2>
-<h1 className={styles.titulo}>Você foi mencionado recentemente:</h1>
-
-<div className={styles.boxTodasOcor}>
-<InicialOcorrencia></InicialOcorrencia>
-<h2 className={styles.h4}>Andamento:</h2>
-<AndamentoOcorrencias></AndamentoOcorrencias>
-
-</div>
-</div>
-
-
-
-
-
-           
+        <div className={styles.boxTodasOcor}>
+          <InicialOcorrencia></InicialOcorrencia>
+          <h2 className={styles.h4}>Andamento:</h2>
+          <AndamentoOcorrencias></AndamentoOcorrencias>
         </div>
+      </div>
+    </div>
+  );
+};
 
-
-
-    )
-}
-
-export default inicial;    
+export default inicial;
