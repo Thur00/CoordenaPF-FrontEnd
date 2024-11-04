6d82d1cd09a2c41502fea8c40ced637da9a4de71
Maria Eduarda <eduarda.maria140806@gmail.com>
2024-11-04 10:44:23 -0300

    .

--- app/Paginas/PaginaInicial/page.jsx
+++ app/Paginas/PaginaInicial/page.jsx
@@ -13,64 +13,56 @@ const API_URL = "http://localhost:3001";
 
 const inicial = () => {
 
-    const [data, setData] = useState([]);
-  
-  
-    const getInicial = async () => {
-      try {
-        const resposta = await fetch(`${API_URL}/alunos`);
-        const data1 = await resposta.json();
-        console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
-        setData(data1);
-        setError(null);
-      } catch (error) {
-        console.error("Erro na busca alunos", error);
-      }
-    };
-  
-    useEffect(() => {
-      getInicial();
-    }, []);
-  
+  const [data, setData] = useState([]);
 
-  
-    return (
-        <div>
-           
 
-            <br></br>
+  const getInicial = async () => {
+    try {
+      const resposta = await fetch(`${API_URL}/alunos`);
+      const data1 = await resposta.json();
+      console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
+      setData(data1);
+      setError(null);
+    } catch (error) {
+      console.error("Erro na busca alunos", error);
+    }
+  };
 
-            <div className={styles.bt}>
+  useEffect(() => {
+    getInicial();
+  }, []);
 
-   <BotaoInicial></BotaoInicial>
+
+
+  return (
+    <div>
+
+
+      <br></br>
+
+      <div className={styles.bt}>
+
+        <BotaoInicial></BotaoInicial>
+
+      </div>
+
+      <br></br>
+      <br></br>
+
+      <div className={styles.tudo}>
+        <h2 className={styles.h4}>Notificações</h2>
+        <h1 className={styles.titulo}>Você foi mencionado recentemente:</h1>
+
+        <div className={styles.boxTodasOcor}>
+          <InicialOcorrencia></InicialOcorrencia>
+          <h2 className={styles.h4}>Andamento:</h2>
+          <AndamentoOcorrencias></AndamentoOcorrencias>
           
-</div>
 
-<br></br>
-<br></br>
-
-<div className={styles.tudo}>
-<h2 className={styles.h4}>Notificações</h2>
-<h1 className={styles.titulo}>Você foi mencionado recentemente:</h1>
-
-<div className={styles.boxTodasOcor}>
-<InicialOcorrencia></InicialOcorrencia>
-<h2 className={styles.h4}>Andamento:</h2>
-<AndamentoOcorrencias></AndamentoOcorrencias>
-
-</div>
-</div>
-
-
-
-
-
-           
         </div>
-
-
-
-    )
+      </div>
+    </div>
+  )
 }
 
 export default inicial;    
