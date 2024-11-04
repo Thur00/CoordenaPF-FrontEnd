a2099a71dd7d778f6f8afa80feea04003fbbe928
AnaBevi <bevilaquaanab@gmail.com>
2024-11-04 13:54:33 -0300

    botão pg inicial

--- Components/BotaoInicial.jsx
+++ Components/BotaoInicial.jsx
@@ -7,14 +7,19 @@ import { CgFileDocument } from "react-icons/cg";
 
 
 function BotaoInicial() {
- return (
+    return (
         <>
-           
-    <Link href="/Paginas/CriarOcorrencia" ><button  className={styles.b1}><CgAdd className={styles.icon}></CgAdd>Criar ocorrência </button></Link>
-    <Link href="/Paginas/TodasOcorrencias" ><button  className={styles.b1}><CgFileDocument className={styles.icon}></CgFileDocument>Ocorrência </button></Link>
-    <Link href="/Paginas/EditarDados" ><button  className={styles.b1}><CgAdd className={styles.icon}></CgAdd>Editar Dados </button></Link>
-         
-          
+            <Link href="/Paginas/CriarOcorrencia" >
+                <button className={styles.b1}> <CgAdd className={styles.icon}> </CgAdd> Criar ocorrência </button>
+            </Link>
+
+            <Link href="/Paginas/TodasOcorrencias" >
+                <button className={styles.b1}><CgFileDocument className={styles.icon}></CgFileDocument>Ocorrência </button>
+            </Link>
+
+            <Link href="/Paginas/EditarDados">
+                <button className={styles.b1}><CgAdd className={styles.icon}></CgAdd>Editar Dados </button>
+            </Link>
         </>
     )
 }
