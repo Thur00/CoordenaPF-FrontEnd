bf2ab9d45a6181073702fb9149d851179d558dfc
Arthur <129216880+Thur00@users.noreply.github.com>
2024-11-05 10:43:03 -0300

    EditarEncaminhamento (#5)
    Co-authored-by: sofiacaunfm <sofiacaumfm@gmail.com>

--- app/Paginas/EditarEncaminhamento/page.jsx
+++ app/Paginas/EditarEncaminhamento/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "@/Components/Adicionar.module.css"; // Importando o CSS
-import Link from "next/link"
+import Link from "next/link";
+
+const API_URL = "http://localhost:3001"; // Adicione a URL da API
 
 const Tabela = () => {
   const [data, setData] = useState([]);
@@ -10,6 +12,21 @@ const Tabela = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [showForm, setShowForm] = useState(false);
   const [editingItem, setEditingItem] = useState(null);
+
+  const getEncaminhamento = async () => {
+    try {
+      const resposta = await fetch(`${API_URL}/encaminhamentos`);
+      const data1 = await resposta.json();
+      console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
+      setData(data1);
+    } catch (error) {
+      console.error("Erro na busca de encaminhamento", error);
+    }
+  };
+
+  useEffect(() => {
+    getEncaminhamento();
+  }, []);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -19,31 +36,73 @@ const Tabela = () => {
   const handleAdd = () => {
     setShowForm(true);
     setIsEditing(false);
-    setFormData({ id: "", encaminhamento: "" });
+    setFormData({ id: "", encaminhamento:"" });
   };
 
   const handleEdit = (item) => {
     setShowForm(true);
     setIsEditing(true);
-    setFormData(item);
+    setFormData({ Encaminhamento_id: item.encaminhamento });
     setEditingItem(item);
   };
 
-  const handleSave = () => {
+  const handleSave = async () => {
     if (isEditing) {
-      setData(
-        data.map((item) => (item.id === editingItem.id ? formData : item))
-      );
+      try {
+        // Faz uma requisição PUT para a API de temas para atualizar o item
+        await fetch(`${API_URL}/encaminhamentos/${editingItem.Encaminhamento_id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ Nome_encaminhamento: formData.encaminhamento }), // Ajuste aqui o objeto para corresponder ao que a API espera
+        });
+
+        // Atualiza a lista de temas após a edição
+        getEncaminhamento();
+
+        // Limpa a seleção e o formulário
+        setEditingItem(null);
+        setIsEditing(false);
+      } catch (error) {
+        console.error("Erro ao atualizar o aspecto:", error);
+      }
     } else {
-      setData([...data, { ...formData, id: Number(formData.id) }]);
+      try {
+        // Faz uma requisição POST para a API de temas
+        const response = await fetch(`${API_URL}/encaminhamentos`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // Envia o corpo da requisição em formato JSON
+          body: JSON.stringify({ Nome_encaminhamento: formData.encaminhamento }),
+        });
+
+        // Atualiza a lista de temas após a edição
+        getEncaminhamento();
+
+        // Converte a resposta para JSON
+        const data = await response.json();
+
+        // Atualiza o estado de 'data' com o novo tema adicionado
+        setData((prevData) => [...prevData, data]);
+
+        // Limpa os campos de entrada
+        setFormData({ id: "", encaminhamento :"" });
+        setShowForm(false);
+      } catch (error) {
+        // Loga erros no console
+        console.error("Erro ao adicionar encaminhamento:", error);
+      }
     }
     setShowForm(false);
-    setFormData({ id: "", encaminhamento: "" });
+    setFormData({ id: "", encaminhamento :"" });
   };
 
   const handleCancel = () => {
     setShowForm(false);
-    setFormData({ id: "", encaminhamento: "" });
+    setFormData({ id: "", encaminhamento :"" });
   };
 
   return (
@@ -51,32 +110,43 @@ const Tabela = () => {
       <br></br>
       <div>
         <div className={styles.div1}>
-          <h1 className={styles.h1}>Adicionar Encaminhamento</h1>
+          <h1 className={styles.h1}>Editar Encaminhamento</h1>
           <button className={styles.voltar}>
             <Link href="/Paginas/EditarDados">Voltar</Link>
-          </button>         </div>
+          </button>
+        </div>
         <br></br>
         <div className={styles.div2}>
           <table border="1" className={styles.table}>
             <thead>
               <tr>
                 <th>ID</th>
-                <th>ENCAMINHAMENTO</th>
+                <th>Encaminhamento</th>
               </tr>
             </thead>
             <tbody>
-              {data.map((item) => (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.encaminhamento}</td>
+              {data.length > 0 ? (
+                data.map((item) => (
+                  <tr key={item.Encaminhamento_id}>
+                    <td>{item.Encaminhamento_id}</td>
+                    <td>{item.Nome_encaminhamento}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="2">Nenhum encaminhamento encontrado.</td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
 
           <div className={styles.buttonContainer}>
             {data.map((item) => (
-              <button className={styles.editarbutton} key={item.id} onClick={() => handleEdit(item)}>
+              <button
+                className={styles.editarbutton}
+                key={item.id}
+                onClick={() => handleEdit(item)}
+              >
                 Editar
               </button>
             ))}
@@ -84,25 +154,32 @@ const Tabela = () => {
         </div>
       </div>
 
-      <button className={styles.addbutton} onClick={handleAdd}>Adicionar</button>
+      <button className={styles.addbutton} onClick={handleAdd}>
+        Adicionar
+      </button>
 
       {showForm && (
         <div>
-          <h3>{isEditing ? "" : ""}</h3>
-          <br></br>
+          <h3 className={styles.titleinput}>
+            {isEditing ? "Editar" : "Adicionar"}
+          </h3>
           <div className={styles.divinput}>
             <input
               type="text"
               name="encaminhamento"
               value={formData.encaminhamento}
               onChange={handleInputChange}
-              placeholder="Nome do encaminhamento"
+              placeholder="encaminhamento"
             />
           </div>
 
           <div className={styles.salecanbutton}>
-            <button className={styles.salvarbutton} onClick={handleSave}>Salvar</button>
-            <button className={styles.cancelarbutton} onClick={handleCancel}>Cancelar</button>
+            <button className={styles.salvarbutton} onClick={handleSave}>
+              Salvar
+            </button>
+            <button className={styles.cancelarbutton} onClick={handleCancel}>
+              Cancelar
+            </button>
           </div>
         </div>
       )}
