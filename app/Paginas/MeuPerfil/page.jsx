da43a3fc7d0c9f2c5c4ce7dc6e34480e6c290ecb
Pyh Quinquio <pyetraquinquio2007@gmail.com>
2024-11-11 17:33:08 -0300

    commit incompleto

--- app/Paginas/MeuPerfil/page.jsx
+++ app/Paginas/MeuPerfil/page.jsx
@@ -4,27 +4,56 @@
 import styles from "@/Components/Perfil.module.css"
 import Link from "next/link"
 
-export default function MeuPerfil(){
-    return(
+export default function MeuPerfil() {
+    return (
         <>
-  
-        <div className={styles.body}>
-        <h1 className={styles.tit}> Meu perfil </h1>
 
-        <div className={styles.input}>
-        <input className={styles.um} type="text" placeholder="Nome:" />
-        <input className={styles.um} type="text" placeholder="E-mail:" />
-        <input className={styles.um} type="text" placeholder="Senha:" />
-        <input className={styles.um} type="text" placeholder="CPF:" />
-        </div>
-       
-        <div className={styles.botoes}>
-        
-        <Link href="/Paginas/PaginaInicial"><button className={styles.botao}> Voltar </button></Link>
-        <button className={styles.botao}> Editar </button>
-        <Link href="/Paginas/Usuarios"><button className={styles.botao2}> Gerenciar Usuários </button></Link>
-        </div></div>
-    
+            <div className={styles.body}>
+                <h1 className={styles.tit}> Meu perfil </h1>
+
+                <div className={styles.input}>
+                    <input className={styles.um} type="text" placeholder="Nome:" />
+                    <input className={styles.um} type="text" placeholder="E-mail:" />
+                    <input className={styles.um} type="text" placeholder="Senha:" />
+                    <input className={styles.um} type="text" placeholder="CPF:" />
+                </div>
+
+                <div className={styles.botoes}>
+
+                    <Link href="/Paginas/PaginaInicial"><button className={styles.botao}> Voltar </button></Link>
+                    <button className={styles.botao}> Editar </button>
+                    <Link href="/Paginas/Usuarios"><button className={styles.botao2}> Gerenciar Usuários </button></Link>
+                </div></div>
+
+        </>
+    )
+} "use client"
+
+
+import styles from "@/Components/Perfil.module.css"
+import Link from "next/link"
+
+export default function MeuPerfil() {
+    return (
+        <>
+
+            <div className={styles.body}>
+                <h1 className={styles.tit}> Meu perfil </h1>
+
+                <div className={styles.input}>
+                    <input className={styles.um} type="text" placeholder="Nome:" />
+                    <input className={styles.um} type="text" placeholder="E-mail:" />
+                    <input className={styles.um} type="text" placeholder="Senha:" />
+                    <input className={styles.um} type="text" placeholder="CPF:" />
+                </div>
+
+                <div className={styles.botoes}>
+
+                    <Link href="/Paginas/PaginaInicial"><button className={styles.botao}> Voltar </button></Link>
+                    <button className={styles.botao}> Editar </button>
+                    <Link href="/Paginas/Usuarios"><button className={styles.botao2}> Gerenciar Usuários </button></Link>
+                </div></div>
+
         </>
     )
 }
