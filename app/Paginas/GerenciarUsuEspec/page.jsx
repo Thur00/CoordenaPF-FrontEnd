888ebad30677a73f760988b8223079640f38b27e
AnaBevi <bevilaquaanab@gmail.com>
2024-11-11 08:21:17 -0300

    alinhar botões

--- app/Paginas/GerenciarUsuEspec/page.jsx
+++ app/Paginas/GerenciarUsuEspec/page.jsx
@@ -1,37 +1,29 @@
 "use client"
 
-
 import styles from "@/Components/Perfil.module.css"
 import Link from "next/link"
 
-export default function Usuario(){
+export default function Usuario() {
 
-   
-
-    return(
+    return (
         <>
 
-      
-        <div className={styles.body}>
-        <h1 className={styles.tit2}>Samara</h1>
-        <p className={styles.p}>Coordenadora Pedagógica</p>
+            <div className={styles.body}>
+                <h1 className={styles.tit2}>Samara</h1>
+                <p className={styles.p}>Coordenadora Pedagógica</p>
 
-        <div className={styles.input}>
-        <input className={styles.um} type="text" placeholder="Nome:" />
-        <input className={styles.um} type="text" placeholder="E-mail:" />
-        <input className={styles.um} type="text" placeholder="Senha:" />
-        <input className={styles.um} type="text" placeholder="CPF:" />
-        
-        </div>
-        <div className={styles.botoes}>
-       <Link  href="/Paginas/Usuarios" ><button className={styles.botao}> Voltar </button></Link>
-        <button className={styles.botao} > Desligar </button>
-     
-        </div></div>
+                <div className={styles.input}>
+                    <input className={styles.um} type="text" placeholder="Nome:" />
+                    <input className={styles.um} type="text" placeholder="E-mail:" />
+                    <input className={styles.um} type="text" placeholder="Senha:" />
+                    <input className={styles.um} type="text" placeholder="CPF:" />
 
-       
+                </div>
+                <div className={styles.botoes}>
+                    <Link href="/Paginas/Usuarios" ><button className={styles.botao}> Voltar </button></Link>
+                    <button className={styles.botao} > Desligar </button>
+                </div></div>
 
         </>
     )
 }
-
