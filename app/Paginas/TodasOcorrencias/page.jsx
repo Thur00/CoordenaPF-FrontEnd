99def3c3271d056a7e473cd278ea0f9918b0856e
Maria Eduarda <eduarda.maria140806@gmail.com>
2024-11-11 16:39:33 -0300

    ;

--- app/Paginas/TodasOcorrencias/page.jsx
+++ app/Paginas/TodasOcorrencias/page.jsx
@@ -1,41 +1,69 @@
 "use client"
 
-import Ocorrencia from "@/Components/Ocorrencia"
 import styles from "./TodasOcorrencias.module.css"
-import Link from "next/link"
 import { IoSearch } from "react-icons/io5";
+import BotaoVoltar from "@/Components/BotaoVoltar";
+import { useEffect, useState } from "react";
+import Ocorrencia from "@/Components/Ocorrencia"
+
+const API_URL = "http://localhost:3001";
+
 
 export default function TodasOcor() {
-    return (
-        <main className={styles.main}>
-          
-            <h1 className={styles.titulo} > Todas as ocorrências </h1>
-            <br></br>
-            <div className={styles.pesquisafiltro}>
-                <div className={styles.pesquisa}>
-                    <input type="text" />
-                    <button> <IoSearch /> </button>
-                </div>
-                <div className={styles.filtro}>
-                    <button className={styles.butfiltro}> RM </button>
-                    <button className={styles.butfiltro}> NOME </button>
-                    <button className={styles.butfiltro}> TEMA </button>
-                    <button className={styles.butfiltro}> DATA </button>
-                    <button className={styles.butfiltro}> STATUS </button>
-                    <button className={styles.butfiltro}> URGÊNCIA </button>
-                </div>
-            </div>
-            <div className={styles.boxTodasOcor}>
-                <Ocorrencia />
-                <Ocorrencia />
-                <Ocorrencia />
-                <Ocorrencia />
-                <Ocorrencia />
-                <Ocorrencia />
-                
-            </div>
-            <br></br>
-            <Link href="/Paginas/PaginaInicial" ><button className="botaovoltar"> Voltar </button></Link>
-        </main>
-    )
+  const [data, setData] = useState([]);
+
+  const getocorrencias = async () => {
+    try {
+      debugger
+      const resposta = await fetch(`${API_URL}/ocorrencias`);
+      const data1 = await resposta.json();
+      console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
+      setData(data1);
+    } catch (error) {
+      console.error("Erro na busca da ocorrência", error);
+    }
+  };
+
+  useEffect(() => {
+    getocorrencias();
+  }, []);
+
+  return (
+    <main className={styles.main}>
+
+      <div className={styles.divtitulo}>
+        <h1 className={styles.titulo} > Todas as ocorrências </h1>
+        <BotaoVoltar link="/Paginas/PaginaInicial" />
+      </div>
+      <br></br>
+      <div className={styles.pesquisafiltro}>
+        <div className={styles.pesquisa}>
+          <input type="text" />
+          <button> <IoSearch /> </button>
+        </div>
+
+
+        <div className={styles.filtro}>
+          <button className={styles.butfiltro}> RM </button>
+          <button className={styles.butfiltro}> NOME </button>
+          <button className={styles.butfiltro}> TEMA </button>
+          <button className={styles.butfiltro}> DATA </button>
+          <button className={styles.butfiltro}> STATUS </button>
+          <button className={styles.butfiltro}> URGÊNCIA </button>
+        </div>
+
+      </div>
+      <div className={styles.boxTodasOcor}>
+          {data.length > 0 ? (
+            data.map((item) => (
+              <>
+                <Ocorrencia nome={item.Criador} turma={item.Turma} tema={item.Tema} data={item.Data} status={item.Status} urgencia={item.Urgencia} />
+              </>
+            ))
+          ) : (<p>Nenhuma ocorrência encontrada</p>)}
+
+        </div>
+
+    </main>
+  )
 }
