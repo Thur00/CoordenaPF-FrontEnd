6db43310305f6107c0c62d967b731fe1e636853d
Maria Eduarda <eduarda.maria140806@gmail.com>
2024-11-11 16:37:06 -0300

    ;

--- app/Paginas/PaginaInicial/page.jsx
+++ app/Paginas/PaginaInicial/page.jsx
@@ -52,26 +52,29 @@ const inicial = () => {
         <h1 className={styles.titulo}>Você foi mencionado recentemente:</h1>
 
         
-          
         <div className={styles.boxTodasOcor}>
-        {data.length > 0 ?(
-          data.map((item) => (
-            <Ocorrencia nome={item.nome} tema={item.tema} data={item.data} status={item.status} urgencia={item.urgencia} />
-          ))
-        ) : (<p>Nenhuma ocorrência encontrada</p>)} 
- 
-         </div>
-          
+          {data.length > 0 ? (
+            data.map((item) => (
+              <>
+                <Ocorrencia nome={item.Criador} turma={item.Turma} tema={item.Tema} data={item.Data} status={item.Status} urgencia={item.Urgencia} />
+              </>
+            ))
+          ) : (<p>Nenhuma ocorrência encontrada</p>)}
+
+        </div>
           <h2 className={styles.h4}>Andamento:</h2>
 
           <div className={styles.boxTodasOcor}>
-        {data.length > 0 ?(
-          data.map((item) => (
-            <Ocorrencia nome={item.nome} tema={item.tema} data={item.data} status={item.status} urgencia={item.urgencia} />
-          ))
-        ) : (<p>Nenhuma ocorrência encontrada</p>)} 
- 
-         </div>
+          {data.length > 0 ? (
+            data.map((item) => (
+              <>
+                <Ocorrencia nome={item.Criador} turma={item.Turma} tema={item.Tema} data={item.Data} status={item.Status} urgencia={item.Urgencia} />
+              </>
+            ))
+          ) : (<p>Nenhuma ocorrência encontrada</p>)}
+
+        </div>
+
 
         </div>
       </div>
