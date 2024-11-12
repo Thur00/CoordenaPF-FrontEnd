e198b2b61f8f8a86ed2b0f3487098796612c3d3a
Maria Eduarda <eduarda.maria140806@gmail.com>
2024-11-12 14:33:48 -0300

    ;

--- Components/Ocorrencias.jsx
+++ Components/Ocorrencias.jsx
@@ -1,32 +1,44 @@
 "user Client"
 
-import styles from "@/app/Paginas/TodasOcorrencias/TodasOcorrencias.module.css"
+import styles from "@/Components/InicialComponentes.module.css"
 import Link from "next/link";
 import { CiCircleCheck } from "react-icons/ci";
-import { CiClock2 } from "react-icons/ci";
-import { CiCircleAlert } from "react-icons/ci";
 
-function Ocorrencia() {
+function Ocorrencia(props) {
+    const { nome, tema, turma, data, urgencia, cor } = props;
+
+    const formattedDate = new Date(data).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
+
+    const getUrgencyClass = () => {
+        if (urgencia === "Muito Urgente") return styles.boxOcor;
+        if (urgencia === "Urgente") return styles.boxOcor2;
+        if (urgencia === "Pouco Urgente") return styles.boxOcor3;
+        return styles.boxOcor; // padrão
+    };
+
     return (
         <>
-            <Link className={styles.boxOcor3} href="/Paginas/PaginaInicial">
-            <div className={styles.a}>
-                <div className={styles.titletematurma}>
-                    <p> Samara te solicitou nessa ocorrência </p>
-                    <p> Tema: Cyberbullying </p>
-                    <p> Turma: 9º ano A </p>
-                </div>
-                <div className={styles.urgenciadatastatus}>
-                    <div className={styles.urgencia3}><p> Muito Urgente </p></div>
-                    <div className={styles.datastatus}>
-                        <p> 03/09/24 </p>
-                        <CiCircleCheck className={styles.icons}/>
+            <Link className={getUrgencyClass()} href="/Paginas/VisualizarOcorrencia">
+                <div className={styles.a}>
+                    <div className={styles.titletematurma}>
+                        <p> Ocorrência criada por {nome} </p>
+                        <p> Tema: {tema} </p>
+                        <p> Turma: {turma} </p>
+                    </div>
+                    <div className={styles.urgenciadatastatus}>
+                        <div className={styles.urgencia1}><p> {urgencia} </p></div>
+                        <div className={styles.datastatus}>
+                            <p>{formattedDate} </p>
+                            <CiCircleCheck className={styles.icons} />
+                        </div>
                     </div>
                 </div>
-            </div>
             </Link>
         </>
-    )
+    );
 }
-
 export default Ocorrencia
