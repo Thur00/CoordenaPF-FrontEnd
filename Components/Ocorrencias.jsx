5da4acea32952bb3842f638b8b67492c04aa0e17
gabyycocielli <129217256+gabyycocielli@users.noreply.github.com>
2024-11-12 11:02:23 -0300

    viz oco

---
+++ Components/Ocorrencias.jsx
@@ -0,0 +1,41 @@
+"user Client"
+
+import styles from "@/Components/InicialComponentes.module.css"
+import Link from "next/link";
+import { CiCircleCheck } from "react-icons/ci";
+
+
+function Ocorrencia(props) {
+    const { nome, tema, turma, data, urgencia} = props
+
+    const formattedDate = new Date(data).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
+
+    
+    return (
+        <>
+            <Link className={styles.boxOcor}  href="/Paginas/VisualizarOcorrencia">
+                <div className={styles.a} >
+                    <div className={styles.titletematurma}>
+                        <p> Ocorrência criada por {nome} </p>
+                        <p> Tema: {tema} </p>
+                        <p> Turma: {turma} </p>
+                       
+                    </div>
+                    <div className={styles.urgenciadatastatus}>
+                        <div className={styles.urgencia1}><p> {urgencia} </p></div>
+                        <div className={styles.datastatus}>
+                            <p>{formattedDate} </p>
+                            <CiCircleCheck className={styles.icons} />
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        </>
+    )
+}
+
+export default Ocorrencia
--- app/Paginas/PaginaInicial/page.jsx
+++ app/Paginas/PaginaInicial/page.jsx
@@ -1,39 +1,48 @@
-"use client";
+"use client"
 
-import styles from "@/Components/InicialComponentes.module.css";
+import styles from "@/Components/InicialComponentes.module.css"
 import React from "react";
-import InicialOcorrencia from "@/Components/InicialOcorrencias";
-import AndamentoOcorrencias from "@/Components/AndamentoOcorrencias";
+import Ocorrencia from "@/Components/Ocorrencias"
 import BotaoInicial from "@/Components/BotaoInicial";
 import { useEffect, useState } from "react";
 
 const API_URL = "http://localhost:3001";
 
+
+
 const inicial = () => {
+
   const [data, setData] = useState([]);
 
-  const getInicial = async () => {
+  const getocorrencias = async () => {
     try {
+      debugger
       const resposta = await fetch(`${API_URL}/ocorrencias`);
       const data1 = await resposta.json();
       console.log("Dados recebidos:", data1); // Adicione esta linha para verificar os dados
       setData(data1);
       setError(null);
     } catch (error) {
-      console.error("Erro na busca alunos", error);
+      console.error("Erro na busca da ocorrência", error);
     }
   };
 
   useEffect(() => {
-    getInicial();
+    getocorrencias();
   }, []);
+
+
 
   return (
     <div>
+
+
       <br></br>
 
       <div className={styles.bt}>
+
         <BotaoInicial></BotaoInicial>
+
       </div>
 
       <br></br>
@@ -43,14 +52,32 @@ const inicial = () => {
         <h2 className={styles.h4}>Notificações</h2>
         <h1 className={styles.titulo}>Você foi mencionado recentemente:</h1>
 
+        
+          
         <div className={styles.boxTodasOcor}>
-          <InicialOcorrencia></InicialOcorrencia>
+        {data.length > 0 ?(
+          data.map((item) => (
+            <Ocorrencia  nome={item.Criador} turma={item.Turma} tema={item.Tema} data={item.Data} status={item.Status} urgencia={item.Urgencia} />
+          ))
+        ) : (<p>Nenhuma ocorrência encontrada</p>)} 
+ 
+         </div>
+          
           <h2 className={styles.h4}>Andamento:</h2>
-          <AndamentoOcorrencias></AndamentoOcorrencias>
+
+          <div className={styles.boxTodasOcor}>
+        {data.length > 0 ?(
+          data.map((item) => (
+            <Ocorrencia  nome={item.Criador} turma={item.Turma} tema={item.Tema} data={item.Data} status={item.Status} urgencia={item.Urgencia} />
+          ))
+        ) : (<p>Nenhuma ocorrência encontrada</p>)} 
+ 
+         </div>
+
         </div>
       </div>
-    </div>
-  );
-};
+
+  )
+}
 
 export default inicial;
