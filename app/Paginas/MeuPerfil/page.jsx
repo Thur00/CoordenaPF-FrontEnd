ccdcae84aba5f40f2a2697c3aac581da0a7016e0
Maria Eduarda <eduarda.maria140806@gmail.com>
2024-11-18 08:50:42 -0300

    ;

--- app/Paginas/MeuPerfil/page.jsx
+++ app/Paginas/MeuPerfil/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-import styles from "@/Components/Perfil.module.css";
 import BotaoVoltar from "@/Components/BotaoVoltar";
 import { useEffect, useState } from "react";
 import styles from "@/Components/Perfil.module.css";
