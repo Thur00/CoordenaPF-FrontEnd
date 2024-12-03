6dfb41c429410ac7e5a2ff3a2cca4b01e3769864
Pyh Quinquio <pyetraquinquio2007@gmail.com>
2024-12-03 08:45:33 -0300

    responsiv.

--- Components/BotaoInicial.jsx
+++ Components/BotaoInicial.jsx
@@ -9,7 +9,7 @@ import { CgFileDocument } from "react-icons/cg";
 function BotaoInicial() {
     return (
         <>
-        <div className={styles.divizona}>
+        
             <Link href="/Paginas/CriarOcorrencia" >
                 <button className={styles.b1}> <CgAdd className={styles.icon}> </CgAdd> Criar ocorrência </button>
             </Link>
@@ -21,7 +21,7 @@ function BotaoInicial() {
             <Link href="/Paginas/EditarDados">
                 <button className={styles.b1}><CgAdd className={styles.icon}></CgAdd>Editar Dados </button>
             </Link>
-            </div>
+            
         </>
     )
 }
